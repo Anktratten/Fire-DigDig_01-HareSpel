a1e61edf0d7f3cd1e612fa32aaf604bf7af61c1d
Anktratten <rasmus.kackeliasson@elev.ga.lbs.se>
2022-02-21 11:00:03 +0100

    Testing script 2

--- DigDig_01_Fire_HareSpel/Assets/Scripts/Testing2.cs
+++ DigDig_01_Fire_HareSpel/Assets/Scripts/Testing2.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Testing : MonoBehaviour
+public class Testing2 : MonoBehaviour
 {
-    public static bool isPaused = false; //Global variabel som styr om spelet är pausat
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        Testing.isPaused = true;
     }
 
     // Update is called once per frame
