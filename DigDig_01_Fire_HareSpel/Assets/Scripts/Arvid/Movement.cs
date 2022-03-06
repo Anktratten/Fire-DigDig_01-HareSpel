29bf6b7eb00630b8968202082af1229fac4bcbf7
Anktratten <rasmus.kackeliasson@elev.ga.lbs.se>
2022-03-06 14:40:29 +0100

    Revert "Update Movement.cs"
    This reverts commit 699516185622579c8f369c0dc1da8e3e5859760d.

--- DigDig_01_Fire_HareSpel/Assets/Scripts/Arvid/Movement.cs
+++ DigDig_01_Fire_HareSpel/Assets/Scripts/Arvid/Movement.cs
@@ -8,17 +8,14 @@ public class Movement : MonoBehaviour
     public Transform bullet;
     public float coolDown = 0.2f;
     float nextTimeToFire = 0;
-    string weaponSelect = "Pistol";
-
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
-<<<<<<< HEAD
     {   if (PauseController.isPaused==false)
         { 
         if (Input.GetKey("right"))
@@ -38,50 +35,10 @@ public class Movement : MonoBehaviour
             transform.position = new Vector3(transform.position.x, transform.position.y - speed * Time.deltaTime, transform.position.z);
         }
         if (Input.GetKey("space") && nextTimeToFire < Time.time)
-=======
-    {
-        if (PauseController.isPaused == false)
->>>>>>> parent of e1e019d (Revert "poijshetidfjstoiafgphgdiuj")
         {
-            if (Input.GetKey("right"))
-            {
-                transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
-            }
-            if (Input.GetKey("left"))
-            {
-                transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
-            }
-            if (Input.GetKey("up"))
-            {
-                transform.position = new Vector3(transform.position.x, transform.position.y + speed * Time.deltaTime, transform.position.z);
-            }
-            if (Input.GetKey("down"))
-            {
-                transform.position = new Vector3(transform.position.x, transform.position.y - speed * Time.deltaTime, transform.position.z);
-            }
-
-            if (Input.GetKey("space") && nextTimeToFire < Time.time)
-            {
-                Instantiate(bullet, new Vector3(transform.position.x + 1, transform.position.y, 0), Quaternion.identity);
-                nextTimeToFire = Time.time + coolDown;
-            }
+            Instantiate(bullet, new Vector3(transform.position.x + 1, transform.position.y, 0), Quaternion.identity);
+            nextTimeToFire = Time.time + coolDown;
+        }
         }
     }
-<<<<<<< HEAD
-=======
-    void PistolSelect()
-    {
-        weaponSelect = "Pistol";
-    }
-
-    void ShotgunSelect()
-    {
-        weaponSelect = "Shotgun";
-    }
-
-    void AutomaticSelect()
-    {
-        weaponSelect = "Automatic";
-    }
->>>>>>> parent of e1e019d (Revert "poijshetidfjstoiafgphgdiuj")
 }
