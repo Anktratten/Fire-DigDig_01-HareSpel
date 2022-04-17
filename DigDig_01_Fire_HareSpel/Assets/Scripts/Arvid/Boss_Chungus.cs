5eb884a6a201b00cbfdda3679b51ba88bcb8c749
ArKaLbs <arvid.karlsson@elev.ga.lbs.se>
2022-04-17 15:46:03 +0200

    inget viktigt

--- DigDig_01_Fire_HareSpel/Assets/Scripts/Arvid/Boss_Chungus.cs
+++ DigDig_01_Fire_HareSpel/Assets/Scripts/Arvid/Boss_Chungus.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
-public class Lives : MonoBehaviour
+public class Boss_Chungus : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
