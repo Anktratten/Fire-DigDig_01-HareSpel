b2ea345d1576f771e58e859901af24720ca9a5b7
ArKaLbs <arvid.karlsson@elev.ga.lbs.se>
2022-04-17 16:14:09 +0200

    SÅ JÄVLA SUR PÅ MIGSJÄLV MED EJ MYCKET GJORT

--- DigDig_01_Fire_HareSpel/Assets/Scripts/Arvid/Boss_Chungus.cs
+++ DigDig_01_Fire_HareSpel/Assets/Scripts/Arvid/Boss_Chungus.cs
@@ -5,6 +5,18 @@ using UnityEngine.UI;
 
 public class Boss_Chungus : MonoBehaviour
 {
+    public int speed = 3;
+    int hp;
+    float delay = 0.5f;
+    float prevSpeed = 1;
+    bool dead;
+    Text ScoreText;
+    public GameObject Player;
+    public Animator animator;
+    public float Cooldown;
+    float nextTimeToFire = 0;
+    public Transform Shot;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +26,16 @@ public class Boss_Chungus : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if (PauseController.isPaused == false)
+        {
+            if (dead == false)
+            {
+            }
+        }
+        else
+        {
+            var animator = GetComponent<Animator>();
+            prevSpeed = animator.speed;
+            animator.speed = 0;
+        }
 }
