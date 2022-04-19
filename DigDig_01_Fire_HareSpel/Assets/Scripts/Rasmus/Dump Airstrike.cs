e16431ff92303ee9ebdb76b329cb94e7ac6c6c96
Anktratten <rasmus.kackeliasson@elev.ga.lbs.se>
2022-04-19 10:30:25 +0200

    Dump airstrike

---
+++ DigDig_01_Fire_HareSpel/Assets/Scripts/Rasmus/Dump Airstrike.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DumpAirstrike : MonoBehaviour
+{
+    public GameObject bomb;
+    int movementSpeed = 10;
+    public GameObject bombDud;
+    public bool isDud;
+    bool bombDropped;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x - movementSpeed * Time.deltaTime, transform.position.y, 0);
+
+        if (transform.position.x < -10)
+        {
+            Destroy(gameObject);
+        }
+        if (transform.position.x > 0 && bombDropped == false)
+        {
+            DropBomb();
+        }
+    }
+    void DropBomb()
+    {
+        if(isDud == false)
+        {
+            Instantiate(bomb, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+        }
+        else
+        {
+            Instantiate(bombDud, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+        }
+    }
+}
