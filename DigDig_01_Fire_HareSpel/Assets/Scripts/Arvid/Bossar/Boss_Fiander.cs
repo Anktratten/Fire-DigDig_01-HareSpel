105e73f4d4d12b3030f94561a10f935fa1a1e5c1
ArKaLbs <arvid.karlsson@elev.ga.lbs.se>
2022-04-19 12:31:13 +0200

    helt eget fixat, bara mig, igen annan

--- DigDig_01_Fire_HareSpel/Assets/Scripts/Arvid/Bossar/Boss_Fiander.cs
+++ DigDig_01_Fire_HareSpel/Assets/Scripts/Arvid/Bossar/Boss_Fiander.cs
@@ -17,6 +17,10 @@ public class Boss_Fiander : MonoBehaviour
     public bool shotgun = false;
     public bool burst = false;
 
+    float bulletCount = 0;
+
+    public float delay;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -50,12 +54,21 @@ public class Boss_Fiander : MonoBehaviour
             {
                 if (nextTimeToFire < Time.time)
                 {
-                    Instantiate(Shot, new Vector3(transform.position.x + 0.1f, transform.position.y, 0), Quaternion.identity);
-                    Instantiate(Shot, new Vector3(transform.position.x + 0.1f, transform.position.y, 0), Quaternion.identity);
-                    Instantiate(Shot, new Vector3(transform.position.x + 0.1f, transform.position.y, 0), Quaternion.identity);
+                    InvokeRepeating("BurstFire", 0, delay /*delay mellan skott*/);
                     nextTimeToFire = Time.time + Cooldown_burst;
                 }
             }
+        }
+    }
+    void BurstFire()
+    {
+        Instantiate(Shot, new Vector3(transform.position.x + 0.1f, transform.position.y, 0), Quaternion.identity);
+        bulletCount++;
+
+        if(bulletCount == 3)
+        {
+            CancelInvoke("BurstFire");
+            bulletCount = 0;
         }
     }
 }
